6f0b03c7d3a101c7e71b2ca3320a0ce4799d492c
yukisako <yukisakoatoz@gmail.com>
2016-06-19 16:34:56 +0900

    [add] made simple validation

--- models/shelter.rb
+++ models/shelter.rb
@@ -4,4 +4,11 @@ ActiveRecord::Base.establish_connection(
 class Shelter < ActiveRecord::Base
   has_secure_password
 
+  #バリデーションに関しては後でもっと詳しくする
+  validates :email,
+    presence: true,
+    format: {with:/.+@.+/}
+  validates :password,
+    length: {in: 5..10}
 end
+
